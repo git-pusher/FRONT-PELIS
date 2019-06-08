5711b689a2f64ecd592940d2e15b5f50029d1d11
git-pusher <rachel.palomeque@gmail.com>
2019-06-08 14:18:42 -0500

    cambios en .gitignore

---
+++ build/precache-manifest.04a4338aa35b3feef04929adbc401a27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487c1d7394dee55b1c8ff5a3d44a4931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e961f16a3cef36dca3cf",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b48c82d1d030e91ae308",
+    "url": "/static/js/2.2c0a3059.chunk.js"
+  },
+  {
+    "revision": "e961f16a3cef36dca3cf",
+    "url": "/static/js/main.782ad69a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
