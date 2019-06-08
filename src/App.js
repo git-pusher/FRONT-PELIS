0af2dea7401239352f9d62cdc21d06d0a31d858f
git-pusher <rachel.palomeque@gmail.com>
2019-06-08 13:40:45 -0500

    faltaba el push del repositorio original XD

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SubirActor from './components/SubirActor';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
