7f6f25cee75dd44bbc1ec7ac932f43250b2d5726
git-pusher <rachel.palomeque@gmail.com>
2019-06-08 13:21:57 -0500

    copiando carpeta front de proyecto mongo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SubirActor from './components/SubirActor';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path="subir-actor" component={SubirActor} />
+      </Router>
+      {/* <SubirActor/> */}
+    </div>
+  );
+}
+
+export default App;
